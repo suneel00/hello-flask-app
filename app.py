b44429f088040c2b3815caa3f411283dc3f7ae53
suneel00 <suneelmadana007@gmail.com>
2025-07-15 17:23:19 +0530

    monitoring

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
