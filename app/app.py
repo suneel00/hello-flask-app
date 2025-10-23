eda6e3807cf78000dab2c412ca169717ae808455
SuneelMadana <60563412+suneel00@users.noreply.github.com>
2025-10-23 18:14:16 +0530

    Update greeting message to 'Hello, World!!'

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, World!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
